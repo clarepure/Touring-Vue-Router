f98ccbd2a2979fec4f6de972d8ff6d45fb6f2c29
Clare <cbeany@purepm.co>
2022-07-18 14:43:27 -0400

    touring-vue-router initialized

---
+++ src/views/EventList.vue
@@ -0,0 +1,64 @@
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <router-link
+      :to="{ name: 'EventList', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+      >Previous Page</router-link
+    >
+    <router-link
+      :to="{ name: 'EventList', query: { page: page + 1 } }"
+      rel="next"
+      v-if="hasNextPage"
+      >Next Page</router-link
+    >
+  </div>
+</template>
+
+<script>
+import { watchEffect } from 'vue'
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  name: 'EventList',
+  props: ['page'],
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null,
+      totalEvents: 0
+    }
+  },
+  created() {
+    watchEffect(() => {
+      EventService.getEvents(2, this.page)
+        .then(response => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
