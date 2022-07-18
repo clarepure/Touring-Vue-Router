1d4dbd58350344a99e1d6404a4fba27fdd71cfd0
Clare <cbeany@purepm.co>
2022-07-18 18:42:56 -0400

    in progress

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,18 @@
 <template>
-  <p>Register for the event here</p>
+  <p>Regstration form here</p>
+  <button @click="register">Register Me!</button>
 </template>
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // If registration API call is successful
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
