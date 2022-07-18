f19ee0905efd90ca2b71b4e33f839a7fc202b7cd
Clare <cbeany@purepm.co>
2022-07-18 15:15:26 -0400

    pagination styling complete

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,18 +2,23 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <router-link
-      :to="{ name: 'EventList', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-      >Previous Page</router-link
-    >
-    <router-link
-      :to="{ name: 'EventList', query: { page: page + 1 } }"
-      rel="next"
-      v-if="hasNextPage"
-      >Next Page</router-link
-    >
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous Page</router-link
+      >
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next Page &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -60,5 +65,24 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
